4565ee54ac05868c34f31ed49bd245b16f23bc1a
ekarin <ekarin.thin@outlook.com>
2019-10-30 11:26:16 +0700

    TodoList

---
+++ src/Components/addItem.components.jsx
@@ -0,0 +1,85 @@
+import React, { Component, useState } from "react";
+import styled from "styled-components";
+import { Button, Form, Box, Heading, Input } from "rimble-ui";
+import ListItem from "./listItem.component";
+
+const Container = styled(Box)`
+  text-align: center;
+  background-color: #2d3748;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`;
+const Header = styled(Heading.h2)`
+  margin: 1rem;
+`;
+const AddList = styled(Form)`
+  display: flex;
+  padding: 24px;
+  margin: auto 36px;
+`;
+
+const AddButton = styled(Button)`
+  border-radius: 8px;
+  font-size: 16px;
+  background-color: pink;
+`;
+
+const Border = styled(Box)`
+  background-color: #488abb;
+  min-height: 540px;
+  width: 480px;
+  border-radius: 8px;
+  margin: auto;
+`;
+
+function AddItem() {
+  const [items, setItems] = useState([]);
+  const [currentItem, setCurrentItem] = useState({ text: "", key: "" });
+
+  const handleInput = props => {
+    setCurrentItem({
+      text: props.target.value,
+      key: Date.now()
+    });
+  };
+
+  const addItem = props => {
+    props.preventDefault();
+    const newItem = currentItem;
+    if (newItem.text !== "") {
+      const newItems = [...items, newItem];
+      setItems(newItems);
+      setCurrentItem({
+        text: "",
+        key: ""
+      });
+    }
+  };
+
+  const deleteItem = key => {
+    const filteredItems = items.filter(item => item.key !== key);
+    setItems(filteredItems);
+  };
+  return (
+    <Container>
+      <Border>
+        <Header>Todo List</Header>
+        <AddList onSubmit={addItem}>
+          <Form.Input
+            placeholder="Add item here"
+            value={currentItem.text}
+            onChange={handleInput}
+          />
+          <AddButton type="submit">Add Item</AddButton>
+        </AddList>
+        <ListItem items={items} deleteItem={deleteItem} />
+      </Border>
+    </Container>
+  );
+}
+
+export default AddItem;
---
+++ src/Components/listItem.component.jsx
@@ -0,0 +1,39 @@
+import React, { Component, useState } from "react";
+import styled from "styled-components";
+import { Button, Form, Box, Heading, Input, Text } from "rimble-ui";
+
+const TextStyle = styled(Text)`
+  color: white;
+  position: relative;
+  padding: 8px;
+`;
+const IconButton = styled(Button.Text)`
+  position: absolute;
+  right: 5px;
+  padding-bottom: 24px;
+  color: #2d3748;
+`;
+const BoxStyle = styled(Box)`
+  background-color: #a6d6f9;
+  margin: 8px 56px;
+  border-radius: 4px;
+`;
+function ListItem(props) {
+  const items = props.items;
+  const listItem = items.map(item => {
+    return (
+      <BoxStyle key={item.key}>
+        <TextStyle>
+          {item.text}
+          <IconButton
+            icon="Delete"
+            onClick={() => props.deleteItem(item.key)}
+          ></IconButton>
+        </TextStyle>
+      </BoxStyle>
+    );
+  });
+  return <Box>{listItem}</Box>;
+}
+
+export default ListItem;
